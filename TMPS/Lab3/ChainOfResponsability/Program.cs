59128bf80b51e89dd8c71bae82415808ce3d966f
david66tanas <95419083+david66tanas@users.noreply.github.com>
2022-05-13 10:46:55 +0300

    Add files via upload

---
+++ TMPS/Lab3/ChainOfResponsability/Program.cs
@@ -0,0 +1,74 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Receiver receiver = new Receiver(false, true, true);
+
+        PaymentHandler bankPaymentHandler = new BankPaymentHandler();
+        PaymentHandler moneyPaymentHadler = new MoneyPaymentHandler();
+        PaymentHandler paypalPaymentHandler = new PayPalPaymentHandler();
+        bankPaymentHandler.Successor = paypalPaymentHandler;
+        paypalPaymentHandler.Successor = moneyPaymentHadler;
+
+        bankPaymentHandler.Handle(receiver);
+
+        Console.Read();
+    }
+}
+
+internal class Receiver
+{
+
+    public bool BankTransfer { get; set; }
+
+    public bool MoneyTransfer { get; set; }
+
+    public bool PayPalTransfer { get; set; }
+
+    public Receiver(bool bt, bool mt, bool ppt)
+    {
+        BankTransfer = bt;
+        MoneyTransfer = mt;
+        PayPalTransfer = ppt;
+    }
+}
+
+internal abstract class PaymentHandler
+{
+    public PaymentHandler Successor { get; set; }
+
+    public abstract void Handle(Receiver receiver);
+}
+
+internal class BankPaymentHandler : PaymentHandler
+{
+    public override void Handle(Receiver receiver)
+    {
+        if (receiver.BankTransfer == true)
+            Console.WriteLine("Facem tranzactie bancara");
+        else if (Successor != null)
+            Successor.Handle(receiver);
+    }
+}
+
+internal class PayPalPaymentHandler : PaymentHandler
+{
+    public override void Handle(Receiver receiver)
+    {
+        if (receiver.PayPalTransfer == true)
+            Console.WriteLine("Facem tranzactie prin PayPal");
+        else if (Successor != null)
+            Successor.Handle(receiver);
+    }
+}
+
+internal class MoneyPaymentHandler : PaymentHandler
+{
+    public override void Handle(Receiver receiver)
+    {
+        if (receiver.MoneyTransfer == true)
+            Console.WriteLine("We carry out transfers through money transfer systems");
+        else if (Successor != null)
+            Successor.Handle(receiver);
+    }
+}
---
+++ TMPS/Lab3/Mediator/Program.cs
@@ -0,0 +1,101 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ManagerMediator mediator = new ManagerMediator();
+        Colleague customer = new CustomerColleague(mediator);
+        Colleague programmer = new ProgrammerColleague(mediator);
+        Colleague tester = new TesterColleague(mediator);
+        mediator.Customer = customer;
+        mediator.Programmer = programmer;
+        mediator.Tester = tester;
+        customer.Send("Avem comanda, trebuie de facut o aplicatie");
+        programmer.Send("Applicatie este gata, ea trebuie sa fie testata");
+        tester.Send("Programa e testata si e gata pentru prod");
+
+        Console.Read();
+    }
+}
+
+internal abstract class Mediator
+{
+    public abstract void Send(string msg, Colleague colleague);
+}
+
+internal abstract class Colleague
+{
+    protected Mediator mediator;
+
+    public Colleague(Mediator mediator)
+    {
+        this.mediator = mediator;
+    }
+
+    public virtual void Send(string message)
+    {
+        mediator.Send(message, this);
+    }
+
+    public abstract void Notify(string message);
+}
+
+internal class CustomerColleague : Colleague
+{
+    public CustomerColleague(Mediator mediator)
+    : base(mediator)
+    { }
+
+    public override void Notify(string message)
+    {
+        Console.WriteLine("Mesaj pentru client: " + message);
+    }
+}
+
+// класс программиста
+internal class ProgrammerColleague : Colleague
+{
+    public ProgrammerColleague(Mediator mediator)
+    : base(mediator)
+    { }
+
+    public override void Notify(string message)
+    {
+        Console.WriteLine("Mesaj pentru programist: " + message);
+    }
+}
+
+// класс тестера
+internal class TesterColleague : Colleague
+{
+    public TesterColleague(Mediator mediator)
+    : base(mediator)
+    { }
+
+    public override void Notify(string message)
+    {
+        Console.WriteLine("Mesaj pentru tester: " + message);
+    }
+}
+
+internal class ManagerMediator : Mediator
+{
+    public Colleague Customer { get; set; }
+    public Colleague Programmer { get; set; }
+    public Colleague Tester { get; set; }
+
+    public override void Send(string msg, Colleague colleague)
+    {
+        // если отправитель - заказчик, значит есть новый заказ
+        // отправляем сообщение программисту - выполнить заказ
+        if (Customer == colleague)
+            Programmer.Notify(msg);
+        // если отправитель - программист, то можно приступать к тестированию
+        // отправляем сообщение тестеру
+        else if (Programmer == colleague)
+            Tester.Notify(msg);
+        // если отправитель - тест, значит продукт готов
+        // отправляем сообщение заказчику
+        else if (Tester == colleague)
+            Customer.Notify(msg);
+    }
+}
